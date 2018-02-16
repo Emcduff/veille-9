6cf64aed8ed1ed89d610906171051b26d7daae84
Edgar McDuff <edgar.mcduff@gmail.com>
2018-02-16 13:38:39 -0500

    Downloaded many packages, changed membres.json to adresses.txt, changed app.js to use express with mongodb

--- 01_app.js
+++ 01_app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const fs = require("fs");
-const app = express();
 const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
+
+
+let db // variable qui contiendra le lien sur la BD
+
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
+ if (err) return console.log(err)
+ db = database.db('carnet_adresse')
+// lancement du serveur Express sur le port 8081
+ app.listen(8081, () => {
+ console.log('connexion à la BD et on écoute sur le port 8081')
+ })
+})
+
 ///////////////////////////////////////////////////////////// Route /html/01_form.html
 app.get('/formulaire', function (req, res) {
  console.log(__dirname);
- res.sendFile( __dirname + "/Public/html/" + "01_form.html" );
+ res.sendFile( __dirname + "/public/html/" + "01_form.html" );
 })
 //////////////////////////////////////////////////////////// Route /
 app.get('/', (req, res) => {
@@ -28,7 +44,7 @@ console.log('la route /traiter_get')
  courriel:req.query.courriel
  };
 
-fs.appendFile(__dirname + "/Public/data/membres.txt", ',' + JSON.stringify(reponse), function (err) {
+fs.appendFile(__dirname + "/public/data/membres.txt", ',' + JSON.stringify(reponse), function (err) {
   if (err) throw err;
   console.log('Sauvegardé');
 });
@@ -36,7 +52,7 @@ fs.appendFile(__dirname + "/Public/data/membres.txt", ',' + JSON.stringify(repon
 })
 ////////////////////////////////////////////////////////// Route : membres
 app.get('/membres', (req,res)=>{
-	fs.readFile( __dirname + "/Public/data/" + "membres.txt", 'utf8', function (err, data) {
+	fs.readFile( __dirname + "/public/data/" + "adresses.txt", 'utf8', function (err, data) {
 		if (err) throw err;
  		let objet = JSON.parse('[' + data + ']');
  		res.end(contenu_objet_json(objet));
@@ -58,10 +74,3 @@ const contenu_objet_json = (o) =>{
 	return info;
 }
 
-var server = app.listen(8081, function () {
-var host = server.address().address
-var port = server.address().port
- 
-console.log("Exemple l'application écoute sur http://%s:%s", host, port)
-
-})
