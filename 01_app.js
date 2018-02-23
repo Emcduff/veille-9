f27687a8b249082c601d9e94918afce382cb1267
Anymuus <e1554575@cmaisonneuve.qc.ca>
2018-02-23 14:09:07 -0500

    Ajout du CSS et modification de 01_app.js pour afficher les routes

--- 01_app.js
+++ 01_app.js
@@ -3,10 +3,11 @@ const fs = require("fs");
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const MongoClient = require('mongodb').MongoClient;
-
-const app = express();
+var util = require("util");
+var app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 
 let db // variable qui contiendra le lien sur la BD
@@ -16,73 +17,46 @@ MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
  db = database.db('carnet_adresse')
 // lancement du serveur Express sur le port 8081
  app.listen(8081, () => {
- console.log('connexion à la BD et on écoute sur le port 8081')
+ console.log('connexion à la BD et on écoute sur le port 8081');
  })
 })
 
-///////////////////////////////////////////////////////////// Route /html/01_form.html
-app.get('/formulaire', function (req, res) {
- res.render('formulaire.ejs');
-})
 //////////////////////////////////////////////////////////// Route /
 app.get('/', (req, res) => {
- console.log('accueil')
- res.end('<h1>Accueil</h1>')
+	console.log('accueil');
+	res.render('accueil.ejs');
 })
+
 /////////////////////////////////////////////////////////// traiter_get
 app.get('/traiter_get', function (req, res) {
  // Preparer l'output en format JSON
+	console.log('la route /traiter_get')
 
-console.log('la route /traiter_get')
-
-// on utilise l'objet req.query pour récupérer les données GET
- let reponse = {
- prenom:req.query.prenom,
- nom:req.query.nom,
- telephone:req.query.telephone,
- courriel:req.query.courriel
- };
-
-fs.appendFile(__dirname + "/public/data/membres.txt", ',' + JSON.stringify(reponse), function (err) {
-  if (err) throw err;
-  console.log('Sauvegardé');
-});
-
+	// on utilise l'objet req.query pour récupérer les données GET
+ 	let reponse = {
+ 	prenom:req.query.prenom,
+ 	nom:req.query.nom,
+	telephone:req.query.telephone,
+ 	courriel:req.query.courriel
+	};
 })
-////////////////////////////////////////////////////////// Route : membres
 
-app.get('/membres', (req, res) => {
- console.log('la route route get / = ' + req.url)
+
+app.get('/adresse', (req, res) => {
+	console.log('la route get / = ' + req.url);
  
- var cursor = db.collection('adresse')
-                .find().toArray(function(err, resultat){
- if (err) return console.log(err)
- // transfert du contenu vers la vue index.ejs (renders)
- // affiche le contenu de la BD
- res.render('membres.ejs', {adresse: resultat})
- }) 
+	var cursor = db.collection('adresse').find().toArray(function(err, resultat){
+	if (err) return console.log(err);
+	res.render('adresse.ejs', {adresse: resultat});
+	});
 })
 
 app.post('/ajouter', (req, res) => {
- db.collection('adresse').save(req.body, (err, result) => {
- if (err) return console.log(err)
- console.log('sauvegarder dans la BD')
- res.redirect('/')
- })
+	db.collection('adresse').save(req.body, (err, result) => {
+	if (err) return console.log(err);
+	console.log('sauvegarder dans la BD');
+	res.redirect('/');
+	});
 })
 
-const contenu_objet_json = (o) =>{
-	let info = '<table>';
-	for(let element of o) {
-
-		for (let p in element)
- 		{ 
-   			info += '<tr><td>' + p + '</td><td>' + element[p] + '</td></tr>';
-   		}
-
-	}
- 	
-	info += '</table>';
-	return info;
-}
 
