b9b249eced619a8adb074762c20bca1c863f17f7
Edgar McDuff <edgar.mcduff@hotmail.ca>
2018-02-23 15:06:19 -0500

    01_app.js, ajout de commentaire

--- 01_app.js
+++ 01_app.js
@@ -8,50 +8,55 @@ const ObjectID = require('mongodb').ObjectID;
 var util = require("util");
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-let db
+let db;
 
+//Établir la connection à la BDD et le serveur
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
 	if (err) return console.log(err);
 	db = database.db('carnet_adresse');
 	app.listen(8081, () => {});
-})
+});
 
+//Route accueil
 app.get('/', (req, res) => {
 	res.render('accueil.ejs');
-})
+});
 
+//Route addresse
 app.get('/adresse', (req, res) => {
 	var cursor = db.collection('adresse').find().toArray(function(err, resultat){
 	if (err) return console.log(err);
 	res.render('adresse.ejs', {adresse: resultat});
 	});
-})
+});
 
+//Ajouter un membre à la BDD
 app.post('/ajouter', (req, res) => {
 	db.collection('adresse').save(req.body, (err, resultat) => {
 	if (err) return console.log(err);
 	console.log('sauvegarder dans la BD');
 	res.redirect('/');
 	});
-})
+});
 
+//Modifier l'information d'un membre sur la BDD
 app.post('/modifier', (req, res) => {
 	req.body._id = ObjectID(req.body._id);
 	db.collection('adresse').save(req.body, (err, resultat) => { 
 		if (err) return console.log(err);
 		res.redirect('/adresse');
 	});
-})
+});
 
+//Supprimer un membre de la BDD
 app.get('/delete/:id', (req, res) => {
 	var id = req.params.id;
 	db.collection('adresse').findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
 		if (err) return console.log(err);
 		res.redirect('/adresse');
 	});
-})
-
+});
